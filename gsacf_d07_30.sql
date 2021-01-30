4c3accc1a4adb675bd58268f9ebc593a58e758ce
morika328 <morika328@gmail.com>
2021-01-30 17:54:48 +0900

    add sql

---
+++ gsacf_d07_30.sql
@@ -0,0 +1,168 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2021-01-30 09:52:04
+-- サーバのバージョン： 10.4.17-MariaDB
+-- PHP のバージョン: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gsacf_d07_30`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `kadai09`
+--
+
+CREATE TABLE `kadai09` (
+  `id` int(12) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `is_admin` int(1) NOT NULL,
+  `is_deleted` int(1) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `like_table`
+--
+
+CREATE TABLE `like_table` (
+  `id` int(12) NOT NULL,
+  `user_id` int(12) NOT NULL,
+  `todo_id` int(12) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `like_table`
+--
+
+INSERT INTO `like_table` (`id`, `user_id`, `todo_id`, `created_at`) VALUES
+(6, 1, 4, '2021-01-16 15:57:40'),
+(7, 1, 4, '2021-01-16 15:57:44'),
+(8, 1, 3, '2021-01-16 16:08:59'),
+(9, 0, 3, '2021-01-16 21:34:50');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `todo_table`
+--
+
+CREATE TABLE `todo_table` (
+  `id` int(12) NOT NULL,
+  `todo` varchar(128) NOT NULL,
+  `deadline` date NOT NULL,
+  `image` varchar(128) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `todo_table`
+--
+
+INSERT INTO `todo_table` (`id`, `todo`, `deadline`, `image`, `created_at`, `updated_at`) VALUES
+(3, 'teat2', '2021-01-09', NULL, '2021-01-09 16:41:11', '2021-01-09 16:41:11'),
+(4, 'teat3', '2021-01-16', NULL, '2021-01-16 15:57:36', '2021-01-16 15:57:36');
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `users_table`
+--
+
+CREATE TABLE `users_table` (
+  `id` int(12) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `is_admin` int(11) NOT NULL,
+  `is_deleted` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `users_table`
+--
+
+INSERT INTO `users_table` (`id`, `username`, `password`, `is_admin`, `is_deleted`, `created_at`, `updated_at`) VALUES
+(1, 'a', 'a', 0, 0, '2021-01-07 23:21:22', '2021-01-07 23:21:22');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `kadai09`
+--
+ALTER TABLE `kadai09`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `like_table`
+--
+ALTER TABLE `like_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `todo_table`
+--
+ALTER TABLE `todo_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- テーブルのインデックス `users_table`
+--
+ALTER TABLE `users_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `kadai09`
+--
+ALTER TABLE `kadai09`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- テーブルの AUTO_INCREMENT `like_table`
+--
+ALTER TABLE `like_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- テーブルの AUTO_INCREMENT `todo_table`
+--
+ALTER TABLE `todo_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- テーブルの AUTO_INCREMENT `users_table`
+--
+ALTER TABLE `users_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
